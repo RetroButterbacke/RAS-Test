78d9ab7b420da49149183bee30dc3391d03c70a4
RetroButterbacke <butterbacke51@gmail.com>
2023-10-11 08:54:50 +0200

    1.3

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 bool isPrime(int n)
 {
     // Corner case
-    if (n <= 1f)
+    if (n <= 1)
         return false;
     // Check from 2 to n-1
     for (int i = 2; i <= n / 2; i++)
@@ -20,28 +20,31 @@ int main () {
 e:
   int e = 0;
   printf("Type in e:\n");
-  scanf("%f", &e);
+  scanf("%d", &e);
   if (!isPrime(e)) { printf("Not a Prime\n"); goto e; }
 p:
   int p = 0;
   printf("Type in p:\n");
-  scanf("%f", &p);
+  scanf("%d", &p);
   if (!isPrime(p)) { printf("Not a Prime\n"); goto p; }
 q:
   int q = 0; 
   printf("Type in q:\n");
-  scanf("%f", &q);
+  scanf("%d", &q);
   if (!isPrime(q)) { printf("Not a Prime\n"); goto q; }
 
   float N = p*q;
-  float d = ((p-1)*(q-1));
+  float d = ((p-1)*(q-1)) + 1;
   if (std::fmod(d, e) != 0.0f) { 
-    d += 1;
-    if (std::fmod((d / e), 1.0f) != 0.0f) {
-      printf("Couldn't calculate key.\n");
-      return 0;
-    } else d /= e;
+    printf("Can't calculate key.\n");
+    return 0;
   } else d /= e;
+
+  if (std::fmod(e*d, ((p-1)*(q-1))) != 1.0f) {
+    printf("Something went wrong.:\(");
+    return 0;
+  }
+
   if (fmod(d, 1.0) != 0.0) return 0;
   printf("public key: %d %d\nprivate key: %d %d\n", (int)e, (int) N, (int)d, (int)N);
   sleep(5);
